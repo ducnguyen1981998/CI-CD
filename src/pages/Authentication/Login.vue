c91898a5c6a76c8393718b66d2b9229bb555523a
1981998 <ducnguyen1981998@gmail.com>
2020-07-17 04:14:02 -0700

    Update Login.vue

--- src/pages/Authentication/Login.vue
+++ src/pages/Authentication/Login.vue
@@ -93,7 +93,7 @@
 
                   <!-- Đăng kí btt -->
                   <md-button to="/register" class="md-primary md-success">
-                    Đăng kiiiiiii
+                    Đăng kí
                   </md-button>
                 </div>
                 <div slot="footer">
